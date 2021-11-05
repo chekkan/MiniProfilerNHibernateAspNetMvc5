eed3162555ef0b2af08b5b0edd2f1eea52de6967
Harish Hareendra Babu <harish.babu@codurance.com>
2021-11-05 21:07:52 +0000

    miniprofiler package added for mvc5 project

---
+++ src/MiniProfilerDemo/Global.asax.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using StackExchange.Profiling;
+using StackExchange.Profiling.Mvc;
+
+namespace MiniProfilerDemo
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            InitProfilerSettings();
+        }
+
+        protected void Application_BeginRequest()
+        {
+            if (Request.IsLocal) MiniProfiler.StartNew();
+        }
+        
+        protected void Application_EndRequest() => MiniProfiler.Current?.Stop();
+
+        private static void InitProfilerSettings()
+        {
+            MiniProfiler.Configure(new MiniProfilerOptions
+                    {
+                        RouteBasePath = "~/profiler",
+                        PopupMaxTracesToShow = 10, // defaults to 15
+                    }
+                    .AddViewProfiling() // Add MVC view profiling
+            );
+        }
+    }
+}
