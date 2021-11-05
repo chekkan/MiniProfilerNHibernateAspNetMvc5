34d22708f69c3896935dfc14c9d120224067e036
Harish Hareendra Babu <harish.babu@codurance.com>
2021-11-05 22:51:18 +0000

    create new employee page

---
+++ src/MiniProfilerDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using MiniProfilerDemo.Infrastructure;
+using MiniProfilerDemo.Models;
+
+namespace MiniProfilerDemo.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public ActionResult New() => View();
+
+        [HttpPost]
+        public async Task<ActionResult> New(Employee newEmployee)
+        {
+            using (var session = NHibernateHelper.OpenSession())
+            {
+                await session.SaveAsync(newEmployee).ConfigureAwait(false);
+                return RedirectToAction("New");
+            }
+        }
+    }
+}
